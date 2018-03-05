251cdf8eaaa4c7537e237236e6381b7bda6f7ac8
sklaassen <36496442+sklaassen@users.noreply.github.com>
2018-03-05 17:56:13 -0500

    Add files via upload

---
+++ EyeTracking/NeuralNet.py
@@ -0,0 +1,105 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
+
+batch_size = 128
+n_classes = 10 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, n_classes])
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def maxpool2d(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+def neural_network_model(data):
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+
+    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+
+    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                    'biases':tf.Variable(tf.random_normal([n_classes])),}
+
+
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']), hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']), hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
+
+    return output
+
+def train_neural_network(x):
+    prediction = convolutional_neural_network(x)
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y,name=None) )
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    
+    hm_epochs = 10
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
+
+            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+def convolutional_neural_network(x):#, keep_rate):
+    weights = {
+        # 5 x 5 convolution, 1 input image, 32 outputs
+        'W_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+        # 5x5 conv, 32 inputs, 64 outputs 
+        'W_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+        # fully connected, 7*7*64 inputs, 1024 outputs
+        'W_fc': tf.Variable(tf.random_normal([7*7*64, 1024])),
+        # 1024 inputs, 10 outputs (class prediction)
+        'out': tf.Variable(tf.random_normal([1024, n_classes]))
+    }
+
+    biases = {
+        'b_conv1': tf.Variable(tf.random_normal([32])),
+        'b_conv2': tf.Variable(tf.random_normal([64])),
+        'b_fc': tf.Variable(tf.random_normal([1024])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+# Reshape input to a 4D tensor 
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+    # Convolution Layer, using our function
+    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
+    # Max Pooling (down-sampling)
+    conv1 = maxpool2d(conv1)
+    # Convolution Layer
+    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
+    # Max Pooling (down-sampling)
+    conv2 = maxpool2d(conv2)
+     # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer
+    fc = tf.reshape(conv2, [-1, 7*7*64])
+    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']) + biases['b_fc'])
+    output = tf.matmul(fc, weights['out']) + biases['out']
+    return output
+
+train_neural_network(x)
---
+++ EyeTracking/main.py
@@ -0,0 +1,115 @@
+import pygame
+import cv2
+import numpy as np
+import os
+import time
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,0)
+BLACK = (0,0,0)
+WHITE =     (255, 255, 255)
+BLUE =      (  0,   0, 255)
+GREEN =     (  0, 255,   0)
+RED =       (255,   0,   0)
+TEXTCOLOR = (  0,   0,  0)
+(width, height) = (1366, 728)
+
+running = True
+#up down left right
+directions = {'up':0,'down':0,'left':0,'right':0}
+lewayPercent = 33/2
+def main():
+	delayTime = 0.01
+	nextTime = time.time()
+	global running, screen
+	cv2.namedWindow("preview")
+	vc = cv2.VideoCapture(0)
+	rval, frame = vc.read()
+	faceCascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
+
+
+	pygame.init()
+	screen = pygame.display.set_mode((width, height))
+	pygame.display.set_caption("eye tracking")
+	screen.fill(BLACK)
+	pygame.display.update()
+
+
+	offset = 20
+	curr=0
+	dest = 1
+	step = 0
+	maxStep = 50
+	pos = [0,0]
+	locks=np.array([[0+offset,0+offset],[width-offset,0+offset],[width-offset,height-offset],[0+offset,height-offset]],np.float32)
+	path, dirs, files = os.walk("./Images/").__next__()
+	counter = len(files)
+
+	while running:
+		ev = pygame.event.get()
+
+		for event in ev:
+			if event.type == pygame.QUIT:
+				running = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				running = False
+
+		screen.fill(BLACK)        
+		
+        
+		cv2.imshow("preview", frame)
+		rval, frame = vc.read()
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30))
+		if len(faces)==1 and time.time() >= nextTime:
+			nextTime = time.time() + delayTime
+			step= step+1
+			if step == maxStep:
+				step = 0
+				curr = dest
+				dest = (dest+1)%len(locks)
+			for (x, y, w, h) in faces:
+				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+				gray = gray[y:y+h, x:x+w]
+				gray = cv2.resize(gray,(100,100))
+
+				if pos[0] <width/2-width*(1/lewayPercent):
+					directions['left'] = 1
+					directions["right"] = 0
+				elif pos[0] >width/2+width*(1/lewayPercent):
+					directions['left'] = 0
+					directions["right"] = 1
+				else:
+					directions['left'] = 0
+					directions["right"] = 0
+
+				if pos[1] <height/2-height*(1/lewayPercent):
+					directions["up"] = 1
+					directions["down"] = 0
+				elif pos[0] >height/2+height*(1/lewayPercent):
+					directions["up"] = 0
+					directions["down"] = 1
+				else:
+					directions["up"] = 0
+					directions["down"] = 0
+
+				filename = str(directions['up'])+"_"+ str(directions['down'])+"_"+ str(directions['left'])+"_" +str(directions['right'])+"_"+ str(counter)
+				cv2.imwrite("./Images/" + filename + ".jpg",gray)
+				counter = counter +1
+						
+		pos = (locks[dest]-locks[curr])/maxStep*step+locks[curr]
+		pygame.draw.circle(screen, BLUE, pos, 20)
+
+
+
+		pygame.display.update()
+	
+
+
+	vc.release()
+	cv2.destroyAllWindows()
+	
+	#release is not actually stoping 
+	vc.stop()
+
+if __name__ == '__main__':
+	main()
+	
