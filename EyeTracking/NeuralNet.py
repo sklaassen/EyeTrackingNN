617bf4b8af0d6e8e0dfa196d1b4e7d72e446efed
stefan <sklaassen@laurentian.ca>
2018-03-13 20:50:53 -0400

    Working CNN
    Got the CNN up and working with MNIST dataset. next need to point it at own generated data

--- EyeTracking/NeuralNet.py
+++ EyeTracking/NeuralNet.py
@@ -2,104 +2,72 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
 
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
-
 batch_size = 128
 n_classes = 10 # MNIST total classes (0-9 digits)
 
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, 784])
+x_shaped = tf.reshape(x, [-1, 28, 28, 1])
 y = tf.placeholder(tf.float32, [None, n_classes])
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def maxpool2d(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_shape, pool_shape, name):
+    # setup the filter input shape for tf.nn.conv_2d
+    conv_filt_shape = [filter_shape[0], filter_shape[1], num_input_channels,
+                      num_filters]
 
-def neural_network_model(data):
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+    # initialise weights and bias for the filter
+    weights = tf.Variable(tf.truncated_normal(conv_filt_shape, stddev=0.03),
+                                      name=name+'_W')
+    bias = tf.Variable(tf.truncated_normal([num_filters]), name=name+'_b')
 
-    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+    # setup the convolutional layer operation
+    out_layer = tf.nn.conv2d(input_data, weights, [1, 1, 1, 1], padding='SAME')
 
-    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+    # add the bias
+    out_layer += bias
 
-    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                    'biases':tf.Variable(tf.random_normal([n_classes])),}
+    # apply a ReLU non-linear activation
+    out_layer = tf.nn.relu(out_layer)
+
+    # now perform max pooling
+    ksize = [1, pool_shape[0], pool_shape[1], 1]
+    strides = [1, 2, 2, 1]
+    out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides, 
+                               padding='SAME')
+
+    return out_layer
+
+layer1 = create_new_conv_layer(x_shaped, 1, 32, [5, 5], [2, 2], name='layer1')
+layer2 = create_new_conv_layer(layer1, 32, 64, [5, 5], [2, 2], name='layer2')
+fc = tf.reshape(layer2, [-1, 7*7*64])
+
+wd1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1000], stddev=0.03), name='wd1')
+bd1 = tf.Variable(tf.truncated_normal([1000], stddev=0.01), name='bd1')
+wd2 = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.03), name='wd2')
+bd2 = tf.Variable(tf.truncated_normal([10], stddev=0.01), name='bd2')
+
+fc = tf.nn.relu(tf.matmul(fc, wd1) + bd1)
+output = tf.matmul(fc, wd2) + bd2
 
 
-    l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
-    l1 = tf.nn.relu(l1)
+cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=output) )
+optimizer = tf.train.AdamOptimizer(0.0001).minimize(cost)
 
-    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']), hidden_2_layer['biases'])
-    l2 = tf.nn.relu(l2)
+hm_epochs = 10
 
-    l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']), hidden_3_layer['biases'])
-    l3 = tf.nn.relu(l3)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
 
-    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
+    for epoch in range(hm_epochs):
+        epoch_loss = 0
+        for _ in range(int(mnist.train.num_examples/batch_size)):
+            epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+            _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+            epoch_loss += c
 
-    return output
+        print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
 
-def train_neural_network(x):
-    prediction = convolutional_neural_network(x)
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y,name=None) )
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
-    
-    hm_epochs = 10
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
+    correct = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
 
-        for epoch in range(hm_epochs):
-            epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-                epoch_loss += c
-
-            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
-
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
-def convolutional_neural_network(x):#, keep_rate):
-    weights = {
-        # 5 x 5 convolution, 1 input image, 32 outputs
-        'W_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-        # 5x5 conv, 32 inputs, 64 outputs 
-        'W_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-        # fully connected, 7*7*64 inputs, 1024 outputs
-        'W_fc': tf.Variable(tf.random_normal([7*7*64, 1024])),
-        # 1024 inputs, 10 outputs (class prediction)
-        'out': tf.Variable(tf.random_normal([1024, n_classes]))
-    }
-
-    biases = {
-        'b_conv1': tf.Variable(tf.random_normal([32])),
-        'b_conv2': tf.Variable(tf.random_normal([64])),
-        'b_fc': tf.Variable(tf.random_normal([1024])),
-        'out': tf.Variable(tf.random_normal([n_classes]))
-    }
-# Reshape input to a 4D tensor 
-    x = tf.reshape(x, shape=[-1, 28, 28, 1])
-    # Convolution Layer, using our function
-    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
-    # Max Pooling (down-sampling)
-    conv1 = maxpool2d(conv1)
-    # Convolution Layer
-    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
-    # Max Pooling (down-sampling)
-    conv2 = maxpool2d(conv2)
-     # Fully connected layer
-    # Reshape conv2 output to fit fully connected layer
-    fc = tf.reshape(conv2, [-1, 7*7*64])
-    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']) + biases['b_fc'])
-    output = tf.matmul(fc, weights['out']) + biases['out']
-    return output
-
-train_neural_network(x)
+    accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+    print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
